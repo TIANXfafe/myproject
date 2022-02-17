a7d4a43644de867712ee1a7433040633dce9c1d7
tianxu <1830015699@qq.com>
2022-02-17 15:01:17 +0800

    新增公共方法

---
+++ src/utils/utils.ts
@@ -0,0 +1,30 @@
+import { useLayoutEffect, useState } from 'react';
+
+/**
+ * 随机生成uuid
+ */
+export function uuid() {
+  const arr: string[] = [];
+  const hexDigits: string = '0123456789abcdef';
+  for (let i: number = 0; i < 36; i++) {
+    arr[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+  }
+  const uniqueId: string = arr.join('');
+  return uniqueId;
+}
+
+/**
+ * 监听视窗宽度变化
+ */
+export function useWindowSize() {
+  const [size, setSize] = useState<[number, number]>([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
