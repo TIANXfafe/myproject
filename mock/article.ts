dd8b4214774b21ea871384bc4a99e7f31a7b97e3
tianxu <1830015699@qq.com>
2022-02-21 17:42:12 +0800

    文章分类mock数据修改

--- mock/article.ts
+++ mock/article.ts
@@ -8,6 +8,74 @@ const waitTime = (time: number = 100) => {
   });
 };
 
+const articleData = [
+  {
+    id: '01',
+    key: '01',
+    name: '一级分类1',
+    article: 10,
+    creator: 'ceshi',
+    createdAt: 1645170247000,
+    status: {
+      flag: true,
+      text: "已发布"
+    },
+    children: [
+      {
+        id: '001',
+        key: '001',
+        name: '二级分类1',
+        article: 100,
+        creator: 'ceshi',
+        createdAt: 1645170247000,
+        status: {
+          flag: false,
+          text: "未发布"
+        },
+        children: [
+          {
+            id: '0001',
+            key: '0001',
+            name: '三级分类1',
+            article: 1000,
+            creator: 'ceshi',
+            createdAt: 1645170247000,
+            status: {
+              flag: true,
+              text: "已发布"
+            },
+          }
+        ]
+      },
+      {
+        id: '002',
+        key: '002',
+        name: '二级分类2',
+        article: 101,
+        creator: 'ceshi',
+        createdAt: 1645170247000,
+        status: {
+          flag: false,
+          text: "未发布"
+        }
+      }
+    ]
+  },
+  {
+    id: '02',
+    key: '02',
+    name: '一级分类2',
+    article: 11,
+    creator: 'ceshi',
+    createdAt: 1645170247000,
+    status: {
+      flag: false,
+      text: "未发布"
+    },
+    children: []
+  }
+]
+
 export default {
   'POST /api/article/classify': async (req: Request, res: Response) => {
     if (Object.keys(req.body).length) {
@@ -17,61 +85,8 @@ export default {
       res.send({
         code: 200,
         message: '成功',
-        data: [
-          {
-            id: '01',
-            key: '01',
-            name: '一级分类1',
-            article: 10,
-            creator: 'ceshi',
-            createdAt: 1645170247000,
-            status: {
-              color: "green",
-              text: "已发布"
-            },
-            children: [
-              {
-                id: '001',
-                key: '001',
-                name: '二级分类1',
-                article: 100,
-                creator: 'ceshi',
-                createdAt: 1645170247000,
-                status: {
-                  color: "red",
-                  text: "未发布"
-                }
-              },
-              {
-                id: '002',
-                key: '002',
-                name: '二级分类2',
-                article: 101,
-                creator: 'ceshi',
-                createdAt: 1645170247000,
-                status: {
-                  color: "red",
-                  text: "未发布"
-                }
-              }
-            ]
-          },
-          {
-            id: '02',
-            key: '02',
-            name: '一级分类2',
-            article: 11,
-            creator: 'ceshi',
-            createdAt: 1645170247000,
-            status: {
-              color: "red",
-              text: "未发布"
-            },
-            children: []
-          }
-        ]
+        data: articleData
       })
     }
-
   }
 }
